d89a4b7c3157f4f47ef49651a61ce5b94f4f02a8
Bautista Ulecia <bautistaulecia@gmail.com>
2022-04-03 18:07:04 -0300

    Timer, barras de vida

--- index.js
+++ index.js
@@ -176,7 +176,38 @@ function animate() {
   player.health-=20
   document.querySelector ("#playerHealth").style.width = player.health + "%"
   }
+  if (enemy.health <=0 || player.health <=0){
+    determineWinner({player, enemy, timerId})
+  }
 }
+
+function determineWinner ({player, enemy, timerId}){
+  clearTimeout (timerId)
+      document.querySelector("#displayText").style.display="flex"
+  if(player.health===enemy.health){
+    document.querySelector("#displayText").innerHTML="Tie"
+  } else if (player.health > enemy.health){
+    document.querySelector("#displayText").innerHTML="Player 1 Wins"
+  } else if (player.health < enemy.health){
+    document.querySelector("#displayText").innerHTML="Player 2 Wins"
+  }
+}
+
+let timer=30
+let timerId
+function decreaseTimer(){
+
+  if (timer>0){
+  timerId = setTimeout (decreaseTimer, 1000)
+  timer--
+  document.querySelector("#timer").innerHTML=timer
+  }
+  if (timer===0) {
+    document.querySelector("#displayText").style.display="flex"
+    determineWinner({player, enemy, timerId})
+  }
+}
+decreaseTimer()
 
 animate()                                                                             /*aca termina el loop que permite la animacion de los rectangulos*/
 
